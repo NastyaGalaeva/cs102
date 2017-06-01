84a44d137ad819b88f091afd77acffb3b995969f
NastyaGalaeva <AAGalaeva@mail.ru>
2017-06-02 00:44:16 +0300

    Add files via upload

---
+++ vkapitag.py
@@ -0,0 +1,121 @@
+from pprint import pprint as pp
+import requests
+from datetime import *
+import csv
+from collections import Counter
+from operator import itemgetter
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
+
+
+q = str(input('Введите слово для поиска '))
+start = int(input('Введите начальное время: '))
+end = int(input('Введите конечное время: ')) #int(datetime.today().timestamp())
+
+
+with open(q + '.csv', 'w', newline='', encoding='UTF-8') as csvfile: #создаю файл на запись CSV формата
+    datawriter = csv.writer(csvfile, delimiter=';',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    datawriter.writerow(['date'] + ['owner_id'] + ['from_id']+ ['post_id'] + ['likes'] + ['reposts'] + ['comments'] #добавляю пераую - названия столбцов
+                        + ['coordinates'] + ['post_type'] + ['platform'] + ['platform_type'])
+
+
+def get_posts():
+    access_token = 'b81336aa4755975fb08b16421221fc8272a82a290f5551e0a2a91e866bd7130d7d9b82d76b97272868647'
+    count = 100 #до 200
+    print('Собираю данные... ')
+    print('TIME: from', datetime.fromtimestamp(start).strftime("%Y-%m-%d %H")+ ' UTC', 'to', datetime.fromtimestamp(end).strftime("%Y-%m-%d %H")+ ':00:00')
+    print('Tag:', q)
+
+    params = {
+        "access_token": access_token,
+        "q": '%23'+str(q),
+        "start_time": start,
+        "end_time": end,
+        "count": count
+    }
+
+    query = "https://api.vk.com/method/newsfeed.search?access_token={access_token}&q={q}&start_time={start_time}&end_time={end_time}&count={count}&version=5.64".format(**params)
+    response = requests.get(query).json()['response']
+    return response
+
+
+def save_posts():
+    response = get_posts()
+    dates=[]
+    owners_id=[]
+    froms_id=[]
+    posts_id=[]
+    likess=[]
+    repostss=[]
+    commentss=[]
+    coordinatess=[]
+    post_types=[]
+    platforms=[]
+    platform_types=[]
+    for i in response[1:]:
+        date = i['date']
+        dates.append(date)
+        owner_id = i['owner_id']  # создатель поста
+        owners_id.append(owner_id)
+        from_id = i['from_id']
+        froms_id.append(from_id)
+        post_id = i['id']  # id поста
+        posts_id.append(post_id)
+        likes = i['likes']['count']  # количество лайков
+        likess.append(likes)
+        reposts = i['reposts']['count']  # количество репостов
+        repostss.append(reposts)
+        comments = i['comments']['count']  # количество комментариев
+        commentss.append(comments)
+        geo = i.setdefault('geo', None)
+        if geo != None:
+            coordinates = geo.setdefault('coordinates', None)  # координаты пользователя
+            coordinatess.append(coordinates)
+        post_type = i['post_type']
+        post_types.append(post_type)
+        platform = i['post_source'].setdefault('platform', 'direct')
+        platforms.append(platform)
+        platform_type = i['post_source'].setdefault('type', None)
+        platform_types.append(platform_type)
+    return dates, owners_id, froms_id, posts_id, likess, repostss, commentss, coordinatess, post_types, platforms, platform_types
+
+
+inf = save_posts()
+date = inf[0]
+for i in range(len(date)):
+     date[i] = datetime.fromtimestamp(date[i]).strftime("%Y-%m-%d %H")+ ':00:00'
+owner_id = inf[1]
+from_id = inf[2]
+post_id = inf[3]
+likes = inf[4]
+reposts = inf[5]
+comments = inf[6]
+coordinates = inf[7]
+post_type = inf[8]
+platform = inf[9]
+platform_type = inf[10]
+with open(str(q) + '.csv', 'a', newline='', encoding='UTF-8') as csvfile:
+    datawriter = csv.writer(csvfile, delimiter=';',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    datawriter.writerow([date]+[owner_id]+ [from_id]+[post_id]+[likes]+[reposts]+[comments]+[coordinates]+[post_type]+[platform]+[platform_type])
+print('Данные сохранены. ')
+
+
+info = Counter(date) #Получаю дату/координату и сколько раз она повторяется
+info = list(info.items())
+info.sort(key=itemgetter(0)) #Сортирую от первой даты/координаты до последней
+key = []
+values =[]
+for i in info:
+    key.append((i[0]))
+    values.append(i[1]) #Кортеж из списка дат/координат и списка повторений
+
+
+plotly.tools.set_credentials_file(username='NastyaGalaev', api_key='4YrbdtqDTzcDPAcjelaF')
+x = key
+y = values
+data = [go.Scatter(x=x,y=y)]
+py.plot(data)
